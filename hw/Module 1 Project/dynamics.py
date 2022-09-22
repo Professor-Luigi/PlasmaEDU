037363073cbbb0b3b5d182aa1c115749c5f59d83
Gio Diaz <gdgd199704@gmail.com>
2022-09-22 18:47:05 -0500

    Updated to use edge detection

--- hw/Module 1 Project/dynamics.py
+++ hw/Module 1 Project/dynamics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from Efield import Efield
 from BorisBunemann import BB
@@ -16,8 +17,10 @@ wall_voltage = 1e3
 gap_length = 0.01 #m
 cap_length = 0.01 #m
 ring_length = 0.02 #m
+wall_thickness = 0.005 #m
+trap_length = 2*gap_length + 2*cap_length + ring_length #m
 
-efield = Efield(R, dr, dz, wall_voltage, gap_length, cap_length, ring_length)
+efield = Efield(R, dr, dz, wall_voltage, gap_length, cap_length, ring_length, wall_thickness, voltage_error_convergence=5e-1)
 
 # Bfield fn
 def Bfield(x, y, z):
@@ -34,8 +37,8 @@ Tc = 2*np.pi/omega_c
 
 # Initial velocity
 vx0 = 0
-vy0 = 0 
-vz0 = 0
+vy0 = 1e6 #m/s 
+vz0 = 1e6 #m/s
 
 # Magnitude of v perp
 v0 = np.sqrt(vx0*vx0 + vy0*vy0)
@@ -50,16 +53,26 @@ x0, y0, z0 = 0, 0, efield.z[efield.first_ring_index] + ring_length/2
 X0 = np.array([x0, y0, z0, vx0, vy0, vz0])
 
 # Time grid
-Nperiods = 2
-Nsteps_per_period = 15
+Nperiods = 500
+Nsteps_per_period = 50
 time = np.linspace(0, Tc*Nperiods, Nperiods*Nsteps_per_period)
 dt = time[1] - time[0]
 
 # Boris Bunemann
-bb = BB(time, X0, q, m, efield.e_interp, Bfield)
+bb = BB(time, X0, q, m, efield.e_interp, Bfield, bounds=[(0,R), (-2*np.pi,2*np.pi), (0,trap_length)], coords='cylindrical', deltas=[dr, 0, dz])
 X_nc = bb.X_nc
+print(X_nc)
 
+plt.figure()
 plt.plot(X_nc[:,0], X_nc[:,1])
+plt.plot(R*np.cos(np.linspace(0,2*np.pi,100)), R*np.sin(np.linspace(0, 2*np.pi,100)))
+plt.xlabel('x (m)')
+plt.ylabel('y (m)')
+
+plt.figure()
+plt.plot(time, X_nc[:,2])
+plt.xlabel('time (s)')
+plt.ylabel('z (m)')    
 plt.show()
 if __name__ == '__main__':
     pass
