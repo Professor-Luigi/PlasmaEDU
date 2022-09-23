d30daafddf5600588648bb712d37d215b0113037
GioD9384 <87088116+GioD9384@users.noreply.github.com>
2022-09-23 11:11:00 -0500

    Still has error where escapes zmax at small but finite z

--- hw/Module 1 Project/dynamics.py
+++ hw/Module 1 Project/dynamics.py
@@ -10,21 +10,21 @@ q = 1.6e-19 #C
 m = 9.11e-31
 
 # Trap dimensions
-R = 0.01 #m
+R = 0.05 #m
 dr = R/100
 dz = 1e-4
 wall_voltage = 1e3
 gap_length = 0.01 #m
-cap_length = 0.01 #m
-ring_length = 0.02 #m
-wall_thickness = 0.005 #m
+cap_length = 0.02 #m
+ring_length = 0.07 #m
+wall_thickness = 0.01 #m
 trap_length = 2*gap_length + 2*cap_length + ring_length #m
 
 efield = Efield(R, dr, dz, wall_voltage, gap_length, cap_length, ring_length, wall_thickness, voltage_error_convergence=5e-1)
 
 # Bfield fn
 def Bfield(x, y, z):
-    B0 = 0.5 #T
+    B0 = 100/10000 #T
     Bx, By, Bz = 0, 0, B0
     return Bx, By, Bz
 
@@ -35,10 +35,14 @@ Bmag = np.sqrt(Bz*Bz + By*By + Bx*Bx)
 omega_c = np.abs(q)*Bmag/m
 Tc = 2*np.pi/omega_c
 
+
+# Thermal Velocity
+v_th = np.sqrt(2*1.38e-23*300/m)
+
 # Initial velocity
-vx0 = 0
-vy0 = 1e6 #m/s 
-vz0 = 1e6 #m/s
+vx0 = v_th 
+vy0 = v_th #m/s 
+vz0 = 0 #m/s
 
 # Magnitude of v perp
 v0 = np.sqrt(vx0*vx0 + vy0*vy0)
@@ -47,32 +51,36 @@ v0 = np.sqrt(vx0*vx0 + vy0*vy0)
 r_L = v0/omega_c
 
 # Initial Position
-x0, y0, z0 = 0, 0, efield.z[efield.first_ring_index] + ring_length/2 
+x0, y0, z0 = 0, R/2, R - wall_thickness*1.25 
+
+# ExB drift velocity
+v_drift = np.linalg.norm(efield.e_interp(x0, y0, z0))/Bmag
+print('v drift and Emag', v_drift, np.linalg.norm(efield.e_interp(x0, y0, z0))) 
 
 # Initial State Vector
 X0 = np.array([x0, y0, z0, vx0, vy0, vz0])
 
 # Time grid
-Nperiods = 500
+Nperiods = 5000
 Nsteps_per_period = 50
 time = np.linspace(0, Tc*Nperiods, Nperiods*Nsteps_per_period)
 dt = time[1] - time[0]
 
 # Boris Bunemann
-bb = BB(time, X0, q, m, efield.e_interp, Bfield, bounds=[(0,R), (-2*np.pi,2*np.pi), (0,trap_length)], coords='cylindrical', deltas=[dr, 0, dz])
+bb = BB(time, X0, q, m, efield.e_interp, Bfield, bounds=[(0,R-wall_thickness), (-2*np.pi,2*np.pi), (0,trap_length)], coords='cylindrical', deltas=[dr, 0, dz])
 X_nc = bb.X_nc
-print(X_nc)
 
-plt.figure()
-plt.plot(X_nc[:,0], X_nc[:,1])
-plt.plot(R*np.cos(np.linspace(0,2*np.pi,100)), R*np.sin(np.linspace(0, 2*np.pi,100)))
-plt.xlabel('x (m)')
-plt.ylabel('y (m)')
+fig, ax = plt.subplots()
+ax.plot(X_nc[:,0], X_nc[:,1])
+ax.plot(R*np.cos(np.linspace(0,2*np.pi,100)), R*np.sin(np.linspace(0, 2*np.pi,100)), color='k')
+ax.set_xlabel('x (m)')
+ax.set_ylabel('y(m)')
+fig.savefig('current_xy.png')
 
-plt.figure()
-plt.plot(time, X_nc[:,2])
-plt.xlabel('time (s)')
-plt.ylabel('z (m)')    
-plt.show()
-if __name__ == '__main__':
-    pass
+fig, ax = plt.subplots()
+ax.plot(time, X_nc[:,2])
+ax.set_xlabel('time (s)')
+ax.set_ylabel('z (m)')
+ax.axhline(0, color='k')
+ax.axhline(trap_length, color='k')
+fig.savefig('current_zt.png')
