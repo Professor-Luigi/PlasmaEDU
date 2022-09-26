60a8a03df64e0f263d2df26ada3ba405a8675227
GioD9384 <87088116+GioD9384@users.noreply.github.com>
2022-09-25 23:31:55 -0500

    Gone through the other required data

--- hw/Module 1 Project/dynamics.py
+++ hw/Module 1 Project/dynamics.py
@@ -11,21 +11,21 @@ q = 1.6e-19 #C
 m = 9.11e-31
 
 # Trap dimensions
-R = 0.05 #m
+R = 0.01 #m
 dr = R/100
 dz = 1e-4
 wall_voltage = 1e3
-gap_length = 0.01 #m
+gap_length = 0.001 #m
 cap_length = 0.02 #m
-ring_length = 0.07 #m
-wall_thickness = 0.01 #m
+ring_length = 0.04 #m
+wall_thickness = 0.005 #m
 trap_length = 2*gap_length + 2*cap_length + ring_length #m
 
 efield = Efield(R, dr, dz, wall_voltage, gap_length, cap_length, ring_length, wall_thickness, voltage_error_convergence=5e-1)
 
 # Bfield fn
 def Bfield(x, y, z):
-    B0 = 100/10000 #T
+    B0 = 0.5 #T
     Bx, By, Bz = 0, 0, B0
     return Bx, By, Bz
 
@@ -35,15 +35,15 @@ Bmag = np.sqrt(Bz*Bz + By*By + Bx*Bx)
 # Cyclotron
 omega_c = np.abs(q)*Bmag/m
 Tc = 2*np.pi/omega_c
-
+print('Tc',Tc)
 
 # Thermal Velocity
 v_th = np.sqrt(2*1.38e-23*300/m)
 
 # Initial velocity
-vx0 = v_th 
-vy0 = v_th #m/s 
-vz0 = 0 #m/s
+vx0 = 3.27e4 
+vy0 = -1.02e5 #m/s 
+vz0 = 5.5e4 #m/s
 
 # Magnitude of v perp
 v0 = np.sqrt(vx0*vx0 + vy0*vy0)
@@ -52,7 +52,7 @@ v0 = np.sqrt(vx0*vx0 + vy0*vy0)
 r_L = v0/omega_c
 
 # Initial Position
-x0, y0, z0 = 0, R/2, R - wall_thickness*1.25 
+x0, y0, z0 = -0.00275, -0.0037, 0.01 
 
 # ExB drift velocity
 v_drift = np.linalg.norm(efield.e_interp(x0, y0, z0))/Bmag
@@ -68,10 +68,11 @@ time = np.linspace(0, Tc*Nperiods, Nperiods*Nsteps_per_period)
 dt = time[1] - time[0]
 
 # Boris Bunemann
-bb = BB(time, X0, q, m, efield.e_interp, Bfield, bounds=[(0,R-wall_thickness), (-2*np.pi,2*np.pi), (0,trap_length)], coords='cylindrical', deltas=[dr, 0, dz])
+bb = BB(time, X0, q, m, efield.e_interp, Bfield, bounds=[(0,R-wall_thickness), (-2*np.pi,2*np.pi), (0,trap_length)], coords='cylindrical', deltas=[dr, 0, dz], save_X_data=False, path=__file__)
 X_nc = bb.X_nc
 
 fig, ax = plt.subplots()
+ax.set_title('XY Plot of the single positron')
 ax.plot(X_nc[:,0], X_nc[:,1])
 ax.plot(R*np.cos(np.linspace(0,2*np.pi,100)), R*np.sin(np.linspace(0, 2*np.pi,100)), color='k')
 ax.set_xlabel('x (m)')
@@ -79,7 +80,15 @@ ax.set_ylabel('y(m)')
 fig.savefig('current_xy.png')
 
 fig, ax = plt.subplots()
+ax.set_title('Zoomed in XY Plot of the single positron')
+ax.plot(X_nc[:,0], X_nc[:,1])
+ax.set_xlabel('x (m)')
+ax.set_ylabel('y(m)')
+fig.savefig('current_xy_zoomed.png')
+
+fig, ax = plt.subplots()
 ax.plot(time, X_nc[:,2])
+ax.set_title('Time evolution of the z position')
 ax.set_xlabel('time (s)')
 ax.set_ylabel('z (m)')
 ax.axhline(0, color='k')
--- hw/Module 1 Project/multiparticleDynamics.py
+++ hw/Module 1 Project/multiparticleDynamics.py
@@ -34,6 +34,18 @@ def checkFiles(path, particle_num, override=False):
 
     return shouldRecalculate
 
+def didEscape(X, dt):
+    escaped = False
+    escape_params = []
+    # Check if the entire state vector is the same for 2 subsequent time steps
+    for i in range(1, len(X[:,0])-1):
+        if np.all(X[i,:] == X[i+1,:]): 
+            escaped = True
+            escape_params.append(dt*i)
+            escape_params.append(X[i,:3])
+            break
+    return escaped, escape_params 
+
 # Trap dimensions
 R = 0.01
 dr = R/100
@@ -48,8 +60,8 @@ trap_length = 2*gap_length + 2*cap_length + ring_length
 # Number of particles
 Nparticles = 20
 
-# Charge parameters for positrons
-q = 1.6e-19
+# Charge parameters for positrons/electron beam
+q = -1.6e-19
 m = 9.11e-31
 
 # Maxwell Dist returns a vx, vy, vz for Nparticles
@@ -57,13 +69,17 @@ def Maxwell(Nparticles, T, m):
     a = np.sqrt(1.38e-23*T/m)
     return ss.norm.rvs(size=Nparticles, scale=a), ss.norm.rvs(size=Nparticles, scale=a), ss.norm.rvs(size=Nparticles, scale=a)
 
+v_th = np.sqrt(2*1.38e-23*300/m)
+v_beam = np.sqrt(2*1.6e-19*(1000)/m) # velocity of beam electrons
+beam_angles = np.linspace(0, np.pi/2, Nparticles)
+
 # Set the initial positions of the particles rows are each particle, columns are x,y,z,vx,vy,vz
 # Particles start somewhere in the ring up to R/2 m away from the center
 X0s = np.zeros((Nparticles, 6))
-X0s[:,0], X0s[:,1], X0s[:,2] = ss.uniform.rvs(size=Nparticles)*R - R/2, ss.uniform.rvs(size=Nparticles)*R- R/2, ss.uniform.rvs(size=Nparticles)*ring_length + cap_length + gap_length 
+X0s[:,0], X0s[:,1], X0s[:,2] = np.ones(Nparticles)*0, np.ones(Nparticles)*0, np.ones(Nparticles)*dz*10 
 
 # Set the initial velocities for the Nparticles
-X0s[:,3], X0s[:,4], X0s[:,5] = Maxwell(Nparticles, 300, m) 
+X0s[:,3], X0s[:,4], X0s[:,5] = np.zeros(Nparticles), v_beam*np.sin(beam_angles), v_beam*np.cos(beam_angles)#np.linspace(0, v_th*7, Nparticles)#Maxwell(Nparticles, 300, m) 
 
 # Efield
 efield = Efield(R, dr, dz, wall_voltage, gap_length, cap_length, ring_length, wall_thickness, voltage_error_convergence=5e-1)
@@ -80,14 +96,16 @@ omega_c = np.abs(q)*Bmag/m
 Tc = 2*np.pi/omega_c
 
 # Time grid
-Nperiods = 500
-Nsteps_per_period = 50
+Nperiods = 5000
+Nsteps_per_period = 10
 time = np.linspace(0, Tc*Nperiods, Nperiods*Nsteps_per_period)
 dt = time[1] - time[0]
 
 #Boris Bunemann
 bb = [] # list of BB instances
 X = [] # list of the X variables
+escaped_bool = [] # whether the particle escaped or not
+escaped_params = [] # time and point of escape
 for i in range(Nparticles):
     if checkFiles(__file__, i, override=False):
         print()
@@ -104,34 +122,102 @@ for i in range(Nparticles):
         saveX(BB_instance.X_c, BB_instance.X_nc, __file__, i)
     else:
         X.append(loadX(__file__, i)[1]) # 1 is non frequency corrected X      
+        single_escaped, single_escaped_params = didEscape(X[-1], dt) 
+        escaped_bool.append(single_escaped)
+        if single_escaped:
+            escaped_params.append(single_escaped_params)
+        else:
+            escaped_params.append((None,None))
+#print(escaped_bool, escaped_params)
+linestyles = ['-', 'dashed', 'dotted', 'dashdot']
 # xy of trap 
-fig, ax = plt.subplots()
+figsize=(14,7)
+fig, ax = plt.subplots(figsize=figsize)
 try:
     for i in range(Nparticles):
-        ax.plot(bb[i].X_nc[:,0], bb[i].X_nc[:,1], label=i)
+        ax.plot(bb[i].X_nc[:,0]*100, bb[i].X_nc[:,1]*100, label=i, ls=linestyles[int(i//10)])
 except:
     for i in range(Nparticles):
-        ax.plot(X[i][:,0], X[i][:,1], label=i)
-ax.plot(R*np.cos(np.linspace(0,2*np.pi,100)), R*np.sin(np.linspace(0,2*np.pi,100)), color='k')
-ax.set_xlabel('x (m)')
-ax.set_ylabel('y (m)')
+        ax.plot(X[i][:,0]*100, X[i][:,1]*100, label=i, ls=linestyles[int(i//10)])
+ax.plot(R*100*np.cos(np.linspace(0,2*np.pi,100)), R*100*np.sin(np.linspace(0,2*np.pi,100)), color='k')
+ax.plot((R - wall_thickness)*100*np.cos(np.linspace(0,2*np.pi,100)), (R - wall_thickness)*100*np.sin(np.linspace(0,2*np.pi,100)), color='gray')
+ax.set_xlabel('x (cm)')
+ax.set_ylabel('y (cm)')
 ax.set_title('XY plane trajectory of 20 positrons')
-ax.legend()
+ax.legend(bbox_to_anchor=(1,1), loc='upper left')
 fig.savefig('XY.png')
 
 # z vs t of trap
-fig, ax = plt.subplots()
+fig, ax = plt.subplots(figsize=figsize)
 try:
     for i in range(Nparticles):
-        ax.plot(time, bb[i].X_nc[:,2], label=i)
+        ax.plot(time, bb[i].X_nc[:,2]*100, label=f'{X0s[i,5]/v_th:.2f}'+'v$_{th}$', ls=linestyles[int(i//10)])
+        #ax.plot(time, bb[i].X_nc[:,2]*100, label=i, ls=linestyles[int(i//10)])
 except:
     for i in range(Nparticles):
-        ax.plot(time, X[i][:,2], label=i)
-ax.axhline(0, color='k')
-ax.axhline(trap_length, color='k')
+        ax.plot(time, X[i][:,2]*100, label=f'{X0s[i,5]/v_th:.2f}'+'v$_{th}$', ls=linestyles[int(i//10)])
+ax.axhline(0, color='k', lw=2)
+ax.axhline((cap_length + gap_length)*100, color='gray', lw=2)
+ax.axhline((cap_length + gap_length + ring_length)*100, color='gray', lw=2)
+ax.axhline(trap_length*100, color='k', lw=2)
 ax.set_xlabel('time (s)')
-ax.set_ylabel('z (m)')
-ax.legend()
+ax.set_ylabel('z (cm)')
+ax.legend(bbox_to_anchor=(1,1), loc='upper left')
 ax.set_title('z vs time trajectory of 20 positrons')
 fig.savefig('zt.png')
+
+# y vs z of beam in trap
+fig, ax = plt.subplots(figsize=figsize)
+try:
+    for i in range(Nparticles):
+        ax.plot(bb[i].X_nc[:,2]*100, bb[i].X_nc[:,1]*100, label=f'{beam_angles[i]*180/np.pi:.1f}'+'\u00B0', ls=linestyles[int(i//10)])
+except:
+    for i in range(Nparticles):
+        ax.plot(X[i][:,2]*100, X[i][:,1]*100, label=f'{beam_angles[i]*180/np.pi:.1f}'+'\u00B0', ls=linestyles[int(i//10)])
+ax.axvline(0, color='k', lw=2)
+ax.axvline(trap_length*100, color='k', lw=2)
+ax.axvline((cap_length + gap_length)*100, color='gray', lw=2)
+ax.axvline((cap_length + gap_length + ring_length)*100, color='gray', lw=2)
+ax.axhline(-R*100, color='k', lw=2)
+ax.axhline(R*100, color='k', lw=2)
+ax.set_xlabel('z (cm)')
+ax.set_ylabel('y (cm)')
+ax.legend(bbox_to_anchor=(1,1), loc='upper left')
+ax.set_title('Incident E beam at various azimuthal angles')
+fig.savefig('yz.png')
+
+# y vs z of beam in trap zoomed in
+fig, ax = plt.subplots(figsize=figsize)
+try:
+    for i in range(Nparticles):
+        ax.plot(bb[i].X_nc[:,2]*100, bb[i].X_nc[:,1]*100, label=f'{beam_angles[i]*180/np.pi:.1f}'+'\u00B0', ls=linestyles[int(i//10)])
+except:
+    for i in range(Nparticles):
+        ax.plot(X[i][:,2]*100, X[i][:,1]*100, label=f'{beam_angles[i]*180/np.pi:.1f}'+'\u00B0', ls=linestyles[int(i//10)])
+ax.set_xlabel('z (cm)')
+ax.set_ylabel('y (cm)')
+ax.set_xlim(0.1, .107)
+ax.legend(bbox_to_anchor=(1,1), loc='upper left')
+ax.set_title('Zoomed in incident E beam at various angles')
+fig.savefig('yz_zoomed.png')
+
+# Plot the amplitude of the electron beam
+fig, ax = plt.subplots(figsize=figsize)
+beam_amplitudes=[]
+try:
+    for i in range(Nparticles):
+        y_max = bb[i].X_nc[:,1].max()
+        y_min = bb[i].X_nc[:,1].min()
+        beam_amplitudes.append(y_max - y_min)
+except:
+    for i in range(Nparticles):
+        y_max = X[i][:,1].max()
+        y_min = X[i][:,1].min()
+        beam_amplitudes.append(y_max - y_min)
+
+ax.plot(beam_angles*180/np.pi, np.array(beam_amplitudes)*100)
+ax.set_xlabel('Azimuthal angle ('+'\u00B0'+')')
+ax.set_ylabel('Beam amplitude (cm)')
+ax.set_title('Beam amplitude vs beam angle')
+fig.savefig('beam amplitudes.png')
 plt.show()
